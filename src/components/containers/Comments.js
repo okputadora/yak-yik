799f4fd68777dc6deaab4bc0f0401fb9124eed60
okputadora <okputadora@gmail.com>
2018-03-12 12:29:03 -0400

    add comments and zones by updating state

--- src/components/containers/Comments.js
+++ src/components/containers/Comments.js
@@ -5,21 +5,55 @@ class Comments extends Component{
   constructor(){
     super()
     this.state = {
+      comment: {
+        username: '',
+        body: '',
+        timestamp: '',
+      },
       list: [
         {body: 'Comment 1', username: 'user1', timestamp: '10:00AM'},
         {body: 'Comment 2', username: 'user2', timestamp: '10:00AM'},
         {body: 'Comment 3', username: 'user3', timestamp: '10:00AM'},
         {body: 'Comment 4', username: 'user4', timestamp: '10:00AM'}
-      ]
+      ],
     }
   }
 
   submitComment(){
-    console.log("submitComment")
+    console.log("submitComment" +JSON.stringify(this.state.comment))
+    let updatedList = Object.assign([], this.state.list)
+    let newComment = Object.assign({}, this.state.comment)
+    updatedList.push(newComment)
+    this.setState({
+      list: updatedList
+    })
   }
-
+  updateBody(event){
+    console.log('updating '+event.target.value)
+    let updatedComment = Object.assign({}, this.state.comment)
+    updatedComment['body'] = event.target.value
+    this.setState({
+      comment: updatedComment
+    })
+  }
   updateUsername(event){
     console.log('updating '+event.target.value)
+    // don't mutate state
+    // WRONGGGGG this.state.comment['username'] = event.target.value
+    // copy it instead
+    let updatedComment = Object.assign({}, this.state.comment)
+    updatedComment['username'] = event.target.value
+    this.setState({
+      comment: updatedComment
+    })
+  }
+
+  updateTimestamp(event){
+    let updatedComment = Object.assign({}, this.state.comment)
+    updatedComment['timestamp'] = event.target.value
+    this.setState({
+      comment: updatedComment
+    })
   }
 
   render(){
@@ -33,9 +67,14 @@ class Comments extends Component{
         <ol>
           {listItems}
         </ol>
-        <input style={universal.marginTop} className="form-control" type="text" name="comment" placeholder="comment"/>
-        <input style={universal.marginTop} onChange={this.updateUsername.bind(this)} className="form-control" type="text" name="username" placeholder="username"/>
-        <button style={universal.marginTop} onClick={this.submitComment.bind(this)} className="btn btn-info">Submit</button>
+        <input style={universal.marginTop} onChange={this.updateBody.bind(this)}
+        className="form-control" type="text" name="comment" placeholder="comment"/>
+        <input style={universal.marginTop} onChange={this.updateUsername.bind(this)}
+        className="form-control" type="text" name="username" placeholder="username"/>
+        <input style={universal.marginTop} onChange={this.updateTimestamp.bind(this)}
+        className="form-control" type="text" name="timestamp" placeholder="timeStamp"/>
+        <button style={universal.marginTop} onClick={this.submitComment.bind(this)}
+        className="btn btn-info">Submit</button>
       </div>
     )
   }
--- src/components/containers/Zones.js
+++ src/components/containers/Zones.js
@@ -4,6 +4,11 @@ class Zones extends Component{
   constructor(){
     super()
     this.state = {
+      zone: {
+        name: '',
+        zipCode: '',
+        comments: ''
+      },
       list: [
         {name: 'Zone 1', zipCode: '10012', comments:10},
         {name: 'Zone 2', zipCode: '10013', comments:20},
@@ -11,6 +16,22 @@ class Zones extends Component{
         {name: 'Zone 4', zipCode: '10015', comments:40}
       ]
     }
+  }
+  updateZone(event){
+    let updatedZone = Object.assign({}, this.state.zone)
+    updatedZone[event.target.id] = event.target.value
+    this.setState({
+      zone: updatedZone
+    })
+  }
+
+  submitZone(){
+    let updatedList = Object.assign([], this.state.list)
+    let newZone = Object.assign([], this.state.zone)
+    updatedList.push(newZone)
+    this.setState({
+      list: updatedList
+    })
   }
   render(){
 
@@ -22,9 +43,13 @@ class Zones extends Component{
         <ol>
           {listItems}
         </ol>
-      </div>
-    )
-  }
-}
+        <input id='name' type='text' onChange={this.updateZone.bind(this)} placeholder='name' className='form-control' name='name'/>
+        <input id='zipCode' type='text' onChange={this.updateZone.bind(this)} placeholder='zip codes' className='form-control' name='zipCode'/>
+        <input id='numComments' type='text' onChange={this.updateZone.bind(this)} placeholder='comments' className='form-control' name='numComments'/>
+        <button onClick={this.submitZone.bind(this)} className='btn btn-info'>Submit Zone</button>
+          </div>
+          )
+          }
+          }
 
 export default Zones
